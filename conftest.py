2c2912fdc30c3956b856c305e72f4d6090702eb9
小石头 <qi284025258@163.com>
2020-07-18 23:24:37 +0800

    20200718 设置chrome浏览器路径

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import ChromeOptions
 from common.constant import constant
-from pages.login import LoginPage
-from pages.home import HomePage
 
 
 @pytest.fixture(scope='class', autouse=True)
 def init_web():
+    # 设置无头浏览器
+    chrome_options = ChromeOptions()
+    chrome_options.binary_location = r'C:\Users\LittleStone\AppData\Local\Google\Chrome\Application\chrome.exe'
     # 启动浏览器
-    driver = webdriver.Chrome(constant.DRIVER_PATH)
+    driver = webdriver.Chrome(constant.DRIVER_PATH, options=chrome_options)
     # 窗口最大化
     driver.maximize_window()
     # 设置隐式等待
